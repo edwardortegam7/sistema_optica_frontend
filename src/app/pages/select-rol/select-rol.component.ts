805645adba68093779b48f9962dce586e5be5aa7
kenneth <126686075+KennethM19@users.noreply.github.com>
2024-07-03 00:21:31 -0500

    kenneth (#2)
    * feat: add selected role page and rol administrativo

---
+++ src/app/pages/select-rol/select-rol.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-select-rol',
+  templateUrl: './select-rol.component.html',
+  styleUrl: './select-rol.component.css'
+})
+export class SelectRolComponent {
+
+}
