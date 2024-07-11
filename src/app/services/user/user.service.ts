35cf959e8513dd7a3d4031ae16d689b1eddd69c5
KennethM19 <ken.jav550@gmail.com>
2024-07-10 21:40:09 -0500

    feat: add api client

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import baseUrl from '../helper';
+import { Observable } from 'rxjs';
+import { TableEmployeesItem } from '../../pages/table-employees/table-employees-datasource';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +13,17 @@ export class UserService {
 
   public añadirUsuario(user:any){
     return this.httpClient.post(`${baseUrl}/usuarios/`, user);
+  }
+
+  public añadirCliente(user:any) {
+    return this.httpClient.post(`${baseUrl}/clientes/`, user);
+  }
+
+  public getCredentials(employee:any) {
+    return this.httpClient.post(`${baseUrl}/login`, employee);
+  }
+
+  public getEmployees(): Observable<any> {
+    return this.httpClient.get(`${baseUrl}/usuarios/get-employees`);
   }
 }
